400521982f4a38e8681f7e7d3a6f00b42eab37f4
Emekwue Innocent <mcinnobezzy@gmail.com>
2023-09-01 21:34:51 +0100

    Portfolio Added

---
+++ portfolio/src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import './portfolio.css'
+import List from './List'
+import Items from './Items'
+import { projects } from '../../Data'
+
+const allNavList = [
+  'all',
+  ...new Set(projects.map((project) => project.category)),
+]
+
+const Portfolio = () => {
+  const [projectItems, setMenuItems] = useState(projects)
+  const [navList, setCategories] = useState(allNavList)
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(projects)
+      return
+    }
+
+    const newProjectItems = projects.filter(
+      (item) => item.category === category
+    )
+
+    setMenuItems(newProjectItems)
+  }
+
+  return (
+    <section className='portfolio section' id='portfolio'>
+      <h2 className='section__title text-cs'>Portfolio</h2>
+      <p className='section__subtitle'>
+        My <span>Cases</span>
+      </p>
+
+      <List list={navList} filterItems={filterItems} />
+
+      <div className='portfolio__container container grid'>
+        <Items projectItems={projectItems} />
+      </div>
+    </section>
+  )
+}
+
+export default Portfolio
