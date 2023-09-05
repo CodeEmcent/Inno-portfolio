5cc7dd220243921df56430e80e1de11e29e8e4bd
Emekwue Innocent <mcinnobezzy@gmail.com>
2023-09-05 21:39:38 +0100

    Footer Component Added

---
+++ portfolio/src/components/footer/Footer.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { FaTwitter, FaDribbble, FaBehance } from 'react-icons/fa'
+
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <div className='footer__container container grid'>
+        <div className='footer__socials'>
+          <a href='#' className='footer__social-link'>
+            <FaTwitter />
+          </a>
+
+          <a href='#' className='footer__social-link'>
+            <FaDribbble />
+          </a>
+
+          <a href='#' className='footer__social-link'>
+            <FaBehance />
+          </a>
+        </div>
+
+        <p className='footer__copyright text-cs'>
+          &copy; 2023 <span>Emcent</span>. All Rights Reserved.
+        </p>
+
+        <p className='footer__copyright text-cs'>
+          Developed by <span>CodeEmcent</span>
+        </p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
